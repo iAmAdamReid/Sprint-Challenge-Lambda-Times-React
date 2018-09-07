01d0803c8078dc8bd60b8f3ed741a2ae52df5194
iAmAdamReid <adam@adamreid.me>
2018-09-07 11:12:54 -0700

    Carousel Stretch Complete

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -5,29 +5,52 @@ export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      index: 0
     }
   }
   componentDidMount(){
-
+    
   }
 
   leftClick = () => {
+    if((this.state.index - 1) < 0){
+      this.setState({
+        index: (carouselData.length - 1)
+      })
+    } else {
+      this.setState({
+        index: this.state.index -= 1
+      })
+    }
 
   }
 
   rightClick = () => {
+    if((this.state.index + 1) > carouselData.length - 1){
+      this.setState({
+        index: 0
+      })
+    } else {
+      this.setState({
+        index: this.state.index += 1
+      })
+    }
 
   }
+
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+   
+    return <img src={carouselData[this.state.index]} style={{display: 'block', Zindex: 0}} />
   }
   
+  
   render(){
+
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        {this.selectedImage()}
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )
