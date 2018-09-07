3eb21edcb83abd3e695256122bb47fd6b1ac35aa
iAmAdamReid <adam@adamreid.me>
2018-09-07 10:00:45 -0700

    Tabs/Cards Complete, Next: PropTypes and Answers.md

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,10 +18,18 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({
+      tabs: tabData,
+      cards: cardData
+    })
   }
 
-  changeSelected = tab => {
+  changeSelected = event => {
+    event.preventDefault();
     // this function should take in the tab and update the state with the new tab.
+    this.setState({
+      selected: event.target.getAttribute('name')
+    })
   };
 
   filterCards = () => {
@@ -37,7 +45,20 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+
+    if(this.state.selected === 'all'){
+      return cardData;
+    } else {
+      const newData = cardData.filter(card => {
+        if(card.tab === this.state.selected){
+          return card;
+        }
+        return newData;
+      });
+      return newData;
+    }
+
+    // return this.state.cards;
   };
 
   render() {
@@ -48,7 +69,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.changeSelected}/>
         <Cards cards={this.filterCards()} />
       </div>
     );
